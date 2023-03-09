28dc6e4f8208615941dd27f306657ba660daaff4
Minseung Jeon <tonyjun00@gmail.com>
2023-03-09 17:10:10 +0900

    multiple time calculation added

--- html/main.js
+++ html/main.js
@@ -1,57 +1,80 @@
 //1. dynamic input blank quantity
-//2. return all subtracted time difference
 
 //later on: group the same tag and result prints it as groups
 
-let time1 = document.getElementById("time1").value;
-let time2 = document.getElementById("time2").value;
+let result = "";
 
-document.getElementById("time1").onchange = function () {
-  diff(time1, time2);
-};
-document.getElementById("time2").onchange = function () {
-  diff(time1, time2);
-};
+function repeat(numberOfTimeInput) {
+  calculate(numberOfTimeInput - 1, numberOfTimeInput, numberOfTimeInput / 2);
 
-function diff(time1, time2) {
-  time1 = document.getElementById("time1").value;
-  time2 = document.getElementById("time2").value;
-  tag1 = document.getElementById("tag1").value;
+  const newCount = numberOfTimeInput - 2;
 
-  time1 = time1.split(":");
-  time2 = time2.split(":");
+  if (newCount > 0) {
+    repeat(newCount);
+  }
+}
 
-  let time1Date = new Date(0, 0, 0, time1[0], time1[1], 0);
-  let time2Date = new Date(0, 0, 0, time2[0], time2[1], 0);
+function calculate(x, y, tagNum) {
+  earlierTime = document.getElementById("time" + x).value;
+  laterTime = document.getElementById("time" + y).value;
+  tag = document.getElementById("tag" + tagNum).value;
 
-  let difference = time2Date.getTime() - time1Date.getTime();
+  earlierTime = earlierTime.split(":");
+  laterTime = laterTime.split(":");
+
+  let earlierDate = new Date(0, 0, 0, earlierTime[0], earlierTime[1], 0);
+  let laterDate = new Date(0, 0, 0, laterTime[0], laterTime[1], 0);
+
+  let difference = laterDate.getTime() - earlierDate.getTime();
   let hours = Math.floor(difference / 1000 / 60 / 60);
   difference = difference - hours * 1000 * 60 * 60;
   let minutes = Math.floor(difference / 1000 / 60);
 
   result =
-    "subtracted time: " +
+    " <br> tag: " +
+    tag +
+    " subtracted time: " +
     (hours < 9 ? "0" : "") +
     hours +
     ":" +
     (minutes < 9 ? "0" : "") +
     minutes +
-    " <br> tag: " +
-    tag1;
-  document.getElementById("output").innerHTML = result;
+    result;
+
+  output(outputOfTime);
 }
 
+function output(outputOfTime) {
+  document.getElementById("ButtonOutput").innerHTML = result;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //--------------------------------------------------------
-
-
+//dynamic input box based on user's input
+/*
 function addInputBoxes() {
   let number = document.getElementById("dynamicNumber").value;
   let container = document.getElementById("container");
-/*
+
   while (container.hasChildNodes()){
     container.removeChild(container.lastChild);
   }
-*/
+
   for (i = 0; i < number; i++) {
     container.appendChild(document.createTextNode(" time " + (i+1) +" "));
     let input = document.createElement("input");
@@ -67,3 +90,4 @@ function addInputBoxes() {
     }
   }
 }
+*/
